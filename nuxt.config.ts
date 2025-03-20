9bd446bcf69e30581af08d921defc27bca0ec4ad
ivan8597 <ivan8597@yandex.ru>
2025-03-20 12:28:23 +0300

    инициализация

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  typescript: {
+    strict: true,
+  },
+  css: ['~/assets/scss/main.scss'],
+  modules: ['@pinia/nuxt'],
+})
